ecf87f96c67c0436d9b65b4e83dcecaec2a322af
Martina Pauer <martinapauer38@gmail.com>
2026-04-02 23:05:28 +0000

    Complete Methods: Only need Write Flask part

--- src/mods/webpauer.py
+++ src/mods/webpauer.py
@@ -24,27 +24,35 @@ class WebPauer:
         '''
             Add a name for the Web App.
         '''
-        pass
+        self.app_name = give_name
 
     def set_style(self, URL: str):
         '''
             Add styles to the Web App.
         '''
-        pass
+        self.stylesheet_URL = URL
 
-    def add_connection(self, con: Connection):
+    def add_connection(self, conn: Connection):
         '''
             Make connection to an API based on
             JSON.
         '''
-        pass
+        self.connector = conn
+        # Select access token from form and API URL from the object
+        self.connector.connect(conn.URL, self.read_from_user('formText'))
 
     def add_product(self, prod: Product):
         '''
             Define a new product for the
             app.
         '''
-        pass
+        if self.products.__len__() >= 3:
+            self.products.append(prod)
+        else:
+            # Save asignaction and only copy needed data to empty Product object slot
+            product_object = self.products[self.products.__len__() - 1]
+            product_object.product_name = prod.get_name()
+            product_object.add_relation(prod.get_prices()[0], prod.get_quality()[0])
 
     def read_from_user(self, category: str):
         '''
@@ -57,17 +65,29 @@ class WebPauer:
             Categories
             
                 - 'formText': data from text entry form
-                - '': file from user device
-                - '': device temperature sensor
-                - '': 
+                - 'devFile': file from user device
+                - 'formOption': selected option from menu
+                - 'getTemp': device temperature sensor
+                - 'cam': WebCam from user
+                - 'mic': User audio Microphone 
         '''
-        pass
-
+        # Detition map for optimize choose one app part by category of data input
+        get_category: dict[str, str] =  {
+                            'formText': self.user_inputs.__setitem__(category, '[TOKEN FROM NEXT ENTRY IN WEB FORM]'),
+                            'devFile': self.user_inputs.__setitem__(category, '[CONTENT FROM FILE]'),
+                            'formOption': self.user_inputs.__setitem__(category, ''),
+                            'cam': self.user_inputs.__setitem__(category, ''),
+                            'mic': self.user_inputs.__setitem__(category, ''),
+                            'getTemp': self.user_inputs.__setitem__(category, '')
+                        }
+        # Save input in dictionary each one is used for selected category use different app parts
+        get_category.get(category)
+        
     def get_name(self) -> str:
         '''
             Said what name has the Web App.
         '''
-        pass
+        return self.app_name
 
     def get_products(self) -> list[Product]:
         '''
@@ -75,11 +95,11 @@ class WebPauer:
             that represents the products from
             the Web App.
         '''
-        pass
+        return self.products
 
     def get_last_JSON(self) -> str:
         '''
             Give the last JSON response from
             connected API.
         '''
-        pass
+        self.connector.get_response()
