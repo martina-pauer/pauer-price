e6fe0db245d6b1db5fd92b5f2b3f8e9594e453d6
Martina Pauer <martinapauer38@gmail.com>
2026-05-08 12:21:36 +0000

    Fix Flask App Mistakes for could be works better

--- src/tango.py
+++ src/tango.py
@@ -85,7 +85,4 @@ del checks
 better.add_relation(selection[0], selection[1])
 del selection
 connector.send_data('{' + f'{better.get_prices()[0]}, {better.get_quality()[0]}' + '}')
-# Listen Port for make the app public with npx
-if __name__ == '__main__':
-    weba.run(host = '0.0.0.0', port = 5000, debug = True)
-    app.add_view(weba)
+app.add_view(weba)
